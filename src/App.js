7825ff50068bad55d294922598a13876969a5fd9
Graphic Design <atosa@Graphics-iMac.local>
2022-02-14 16:15:57 -0800

    added npm packages and new SRC folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hero from './components/hero/Hero';
+import About from './components/about/About';
+import Portfolio from './components/portfolio/Portfolio';
+import Quilt from './assets/images/darkquilt.png';
+import { Parallax } from "react-parallax";
+import styled, { createGlobalStyle } from 'styled-components';
+import Colombia from './assets/images/colombia2017.png';
+import Waikiki from './assets/images/Waikiki.png';
+import './App.css'
+
+const GlobalStyle = createGlobalStyle`
+    html {
+      box-sizing: border-box;
+    }
+    *, *:before, *:after {
+      box-sizing: inherit;
+      color: #fff
+    } 
+    
+`;
+
+const Background = styled.div`
+    background-image: url(${Quilt});
+    color: #fff;
+`;
+const Container = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Background>
+      <GlobalStyle />
+          <Hero />
+          <Container>
+            <About />
+            </Container>
+              <Parallax bgImage={Colombia} strength={900}>
+                <div style={{ height: 350 }}>
+                </div>
+              </Parallax>
+            <Container>
+            <Portfolio />
+            </Container>
+            <Parallax bgImage={Waikiki} strength={900}>
+                <div style={{ height: 350 }}>
+                </div>
+              </Parallax>
+        </Background>
     </div>
   );
 }
---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,166 @@
+import React from "react";
+import TrynaEat from "../../assets/images/TrynaEat.png";
+import GiphyAPI from "../../assets/images/GiphyAPI.png";
+import UrbanLA from "../../assets/images/UrbanLAStudio.png";
+import Cartographer from "../../assets/images/cartographer1.png";
+import { PopIn } from "react-spring-pop";
+import styled from "styled-components";
+import "./portfolio.css";
+import CookingWithReact from "../../assets/images/cooking-with-react.png";
+
+const HeaderText = styled.h1`
+  padding-top: 40px;
+  text-align: center;
+  font-size: 3em;
+`;
+const ProjectTitle = styled.h2`
+  color: #fff;
+`;
+const UlText = styled.ul`
+  list-style: none;
+`;
+const LiText = styled.li`
+  margin-right: 2.5em;
+`;
+const Line = styled.hr`
+  width: 50%;
+  margin-bottom: 40px;
+  opactiy: 60%;
+  border: 1px solid grey;
+`;
+const PortfolioContainer = styled.div`
+  background-image: url(${Cartographer});
+  border-radius: 40px;
+  -webkit-box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+`;
+const Featured = styled.img`
+  max-width: 1050px;
+  width: 100%;
+  border-radius: 50px;
+  -webkit-box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  margin-bottom: 20px;
+`;
+const Icons = styled.img`
+  max-width: 100%;
+  border-radius: 41px;
+  -webkit-box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+  box-shadow: 4px 7px 24px 4px rgba(0, 0, 0, 0.19);
+`;
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding-bottom: 40px;
+`;
+const Portfolio = () => {
+  return (
+    <PortfolioContainer>
+      <HeaderText>Portfolio</HeaderText>
+      <div class="portfolio">
+        <Line />
+        <PopIn tension="500" once="true" threshold="1" mass="5">
+          <div class="content">
+            <a href="https://sleepy-mclean-fd063d.netlify.app/" target="_blank">
+              <div class="content-overlay"></div>
+              <Featured class="content-image" src={CookingWithReact} />
+              <div class="content-details fadeIn-top">
+                <ProjectTitle>Cooking With React</ProjectTitle>
+                <h4>
+                  <strong>Main Objective:</strong>
+                </h4>
+                <p class="objective_text">
+                  Create a list of recipes stored on the local storage. You're
+                  able to create, read, update, and delete stored data. This
+                  application is also a continuous deployment application hosted
+                  on Netlify.
+                </p>
+                <h4>
+                  <strong>Technolgies Used:</strong>
+                </h4>
+                <UlText>
+                  <LiText>ReactJS</LiText>
+                  <LiText>React Hooks</LiText>
+                  <LiText>Local Storage</LiText>
+                  <LiText>Netlify</LiText>
+                  <LiText>CSS Grid</LiText>
+                  <LiText>UUID for Unique Keys</LiText>
+                </UlText>
+              </div>
+            </a>
+          </div>
+        </PopIn>
+        <PopIn tension="500" once="true" threshold="1" mass="5">
+          <div class="content">
+            <a
+              href="https://asaquilabon.github.io/Random-Food-Button/"
+              target="_blank"
+            >
+              <div class="content-overlay"></div>
+              <Featured class="content-image" src={TrynaEat} />
+              <div class="content-details fadeIn-top">
+                <ProjectTitle>Tryna Eat?!</ProjectTitle>
+                <h4>
+                  <strong>Main Objective:</strong>
+                </h4>
+                <p class="objective_text">
+                  Eliminate the day to day issue of not ever knowing what to eat
+                </p>
+                <h4>
+                  <strong>Technolgies Used:</strong>
+                </h4>
+                <UlText>
+                  <LiText>ReactJS</LiText>
+                  <LiText>Yelp Fusion API</LiText>
+                  <LiText>AWS Lambda Proxy Integrations for CORS </LiText>
+                  <LiText>Axios</LiText>
+                </UlText>
+              </div>
+            </a>
+          </div>
+        </PopIn>
+        <Bottom>
+          <PopIn tension="100" once="true" threshold="1" mass="3">
+            <div class="content">
+              <a href="https://asaquilabon.github.io/GiphyAPI/" target="_blank">
+                <div class="content-overlay"></div>
+                <Icons class="content-image" src={GiphyAPI} />
+                <div class="content-details fadeIn-top">
+                  <h3>Giphy API</h3>
+                  <h4>Technolgies Used:</h4>
+                  <UlText>
+                    <LiText>HTML + CSS</LiText>
+                    <LiText>Giphy API</LiText>
+                    <LiText>Javacript + JQuery</LiText>
+                    <LiText>AJAX</LiText>
+                  </UlText>
+                </div>
+              </a>
+            </div>
+          </PopIn>
+          <PopIn tension="100" once="true" threshold="1" mass="3">
+            <div class="content">
+              <a href="http://urbanlastudio.com/" target="_blank">
+                <div class="content-overlay"></div>
+                <Icons class="content-image" src={UrbanLA} />
+                <div class="content-details fadeIn-top">
+                  <h3>UrbanLAStudio.com</h3>
+                  <h4>Technolgies Used:</h4>
+                  <UlText>
+                    <LiText>HTML + CSS</LiText>
+                    <LiText>Javascript</LiText>
+                    <LiText>W3 Schools CSS</LiText>
+                  </UlText>
+                </div>
+              </a>
+            </div>
+          </PopIn>
+        </Bottom>
+      </div>
+    </PortfolioContainer>
+  );
+};
+export default Portfolio;
